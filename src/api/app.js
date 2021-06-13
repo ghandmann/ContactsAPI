859620e5766702d4d89b65c2a2369e8553817845
Sven Eppler <sven.eppler@sodgeit.de>
2021-06-13 17:22:18 +0200

    implement minimal welcome screen

--- src/api/app.js
+++ src/api/app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var homeRouter = require('./routes/home');
 var apiRouter = require("./routes/api");
 
 var app = express();
@@ -13,6 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', homeRouter);
 app.use('/', apiRouter);
 
 module.exports = app;
